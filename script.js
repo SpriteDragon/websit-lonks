9a02807173929eb63fc478c59fced177c3ffd323
SpriteDragon <135884372+SpriteDragon@users.noreply.github.com>
2024-09-17 19:55:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+        function websiteone() { const w = window.open() // access the "about:blank" window you've opened
+w.document.body.innerHTML = "<title>Google Docs</title><link href='favicon.ico'><iframe src='link1.html' frameborder='0' style='height:100%;width:100%;frameborder:0;'>"
+// or access other parts to add more JS or CSS
+const style = w.document.createElement("link")
+link.href = "/path/to.css"
+link.rel = "stylesheet"
+w.document.head.appendChild(style) }
+    
+            function websitetwo() { const w = window.open() // access the "about:blank" window you've opened
+w.document.body.innerHTML = "<title>Gamez!</title><iframe src='link2.html' frameborder='0' style='height:100%;width:100%;frameborder:0;'>"
+// or access other parts to add more JS or CSS
+const style = w.document.createElement("link")
+link.href = "/path/to.css"
+link.rel = "stylesheet"
+w.document.head.appendChild(style) }
+    
+            function websitethree() { const w = window.open() // access the "about:blank" window you've opened
+w.document.body.innerHTML = "<title>Gamez!</title><iframe src='link3.html' frameborder='0' style='height:100%;width:100%;frameborder:0;'>"
+// or access other parts to add more JS or CSS
+const style = w.document.createElement("link")
+link.href = "/path/to.css"
+link.rel = "stylesheet"
+w.document.head.appendChild(style) }
+
+function websitefour() { const w = window.open() // access the "about:blank" window you've opened
+    w.document.body.innerHTML = "<title>Gamez!</title><iframe src='link4.html' frameborder='0' style='height:100%;width:100%;frameborder:0;'>"
+    // or access other parts to add more JS or CSS
+    const style = w.document.createElement("link")
+    link.href = "/path/to.css"
+    link.rel = "stylesheet"
+    w.document.head.appendChild(style) }
